2e9cb23a2345329f256ecebb86c26344cafdedcd
Rod C. <rodcad.808@gmail.com>
2026-09-23 15:57:01 -1000

    w

---
+++ ziplist.js
@@ -0,0 +1,12 @@
+"use strict";
+const testArray1 = [1, 2, 3];
+const testArray2 = ['a', 'b', 'c'];
+function zipList(listArray1, listArray2) {
+    const result = [];
+    for (let i = 0; i < listArray1.length; i++) {
+        result.push(listArray1[i]);
+        result.push(listArray2[i]);
+    }
+    return result;
+}
+console.log(zipList(testArray1, testArray2));
---
+++ ziplist.ts
@@ -0,0 +1,13 @@
+const testArray1 = [1, 2, 3];
+const testArray2 = ['a', 'b', 'c'];
+
+function zipList(listArray1: (number | string)[], listArray2: (number | string)[]): (number | string)[] {
+  const result: (number | string)[] = [];
+  for (let i = 0; i < listArray1.length; i++) {
+    result.push(listArray1[i]);
+    result.push(listArray2[i]);
+  }
+  return result;
+}
+
+console.log(zipList(testArray1, testArray2));
